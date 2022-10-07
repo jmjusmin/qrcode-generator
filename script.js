e160903915cc5fc3d7674204435a0a68ec4d52ed
jmjusmin <jmjusmin1412@gmail.com>
2022-10-07 16:11:01 -0700

    Implement the dropDown and link

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-"use strict";
+'use strict';
+// declare variable
+const linkEl = document.getElementById('inputLink').value;
+const img = document.getElementById('output--img');
+const dropDownEl = document.getElementById('size');
+let sizeSelected;
+const radioEl = document.querySelector(
+  "input[type='radio'][name='bg_choice']:checked"
+);
+let bgSelected;
+const searchBtn = document.querySelector('.btn--main');
+const downloadBtn = document.querySelector('.btn--download');
+const outputQR = document.querySelector('.output--qr');
+
+// action when click
+searchBtn.addEventListener('click', () => {
+  sizeSelected = dropDownEl.value;
+  bgSelected = radioEl.value;
+  console.log(radioEl);
+  //validation input
+  img.src = `https://qrtag.net/api/qr_transparent_${sizeSelected}.png?url=${linkEl}`;
+  //if not
+  outputQR.classList.toggle('hidden');
+});
+
+downloadBtn.addEventListener('click', (url) => {});
