9870cfefa82656062e0de0849ac94826f182f246
jmjusmin <jmjusmin1412@gmail.com>
2022-10-08 14:05:21 -0700

    Completed basic function

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 'use strict';
 // declare variable
-const linkEl = document.getElementById('inputLink').value;
+const linkEl = document.querySelector('#inputLink');
 const img = document.getElementById('output--img');
-const dropDownEl = document.getElementById('size');
-let sizeSelected;
-const radioEl = document.querySelector(
-  "input[type='radio'][name='bg_choice']:checked"
-);
+const sizeSelected = document.getElementById('size');
+const radioEl = document.getElementsByName('bg_choice');
 let bgSelected;
 const searchBtn = document.querySelector('.btn--main');
 const downloadBtn = document.querySelector('.btn--download');
 const outputQR = document.querySelector('.output--qr');
+const outputQRError = document.querySelector('.output--qr-error');
 
 // action when click
 searchBtn.addEventListener('click', () => {
-  sizeSelected = dropDownEl.value;
-  bgSelected = radioEl.value;
-  console.log(radioEl);
+  console.log(linkEl);
+  console.log(typeof linkEl.value);
   //validation input
-  img.src = `https://qrtag.net/api/qr_transparent_${sizeSelected}.png?url=${linkEl}`;
-  //if not
-  outputQR.classList.toggle('hidden');
+  if (linkEl.value.length !== 0) {
+    bgSelected = Array.from(radioEl).find((radio) => radio.checked).value;
+    img.src = `https://qrtag.net/api/qr${bgSelected}${sizeSelected.value}.png?url=${linkEl.value}`;
+    //if not
+    outputQR.classList.toggle('hidden');
+  } else {
+    outputQRError.classList.toggle('hidden');
+  }
 });
 
 downloadBtn.addEventListener('click', (url) => {});
